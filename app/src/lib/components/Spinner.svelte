947cb86f79b707d4605674f5c8ea9bb99508ab5d
Torsten Sprenger <mail@spren9er.de>
2022-04-13 11:46:31 +0200

    Update spinner color

--- app/src/lib/components/Spinner.svelte
+++ app/src/lib/components/Spinner.svelte
@@ -1,44 +1,41 @@
 <svg class="spinner" viewBox="0 0 40 40">
-  <circle class="path" cx="20" cy="20" r="16" fill="none" stroke-width="3" />
+	<circle class="path" cx="20" cy="20" r="16" fill="none" stroke-width="3" />
 </svg>
 
 <style>
-  .spinner {
-    animation: rotate 2s linear infinite;
-    z-index: 2;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin: -20px 0 0 -20px;
-    width: 40px;
-    height: 40px;
-  }
-
-  .spinner .path {
-    stroke: #222222;
-    opacity: 0.6;
-    stroke-linecap: round;
-    animation: dash 1.5s ease-in-out infinite;
-  }
-
-  @keyframes rotate {
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-
-  @keyframes dash {
-    0% {
-      stroke-dasharray: 1, 150;
-      stroke-dashoffset: 0;
-    }
-    50% {
-      stroke-dasharray: 90, 150;
-      stroke-dashoffset: -35;
-    }
-    100% {
-      stroke-dasharray: 90, 150;
-      stroke-dashoffset: -124;
-    }
-  }
+	.spinner {
+		animation: rotate 2s linear infinite;
+		z-index: 2;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		margin: -20px 0 0 -20px;
+		width: 40px;
+		height: 40px;
+	}
+	.spinner .path {
+		stroke: #ffffff;
+		opacity: 0.6;
+		stroke-linecap: round;
+		animation: dash 1.5s ease-in-out infinite;
+	}
+	@keyframes rotate {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+	@keyframes dash {
+		0% {
+			stroke-dasharray: 1, 150;
+			stroke-dashoffset: 0;
+		}
+		50% {
+			stroke-dasharray: 90, 150;
+			stroke-dashoffset: -35;
+		}
+		100% {
+			stroke-dasharray: 90, 150;
+			stroke-dashoffset: -124;
+		}
+	}
 </style>
