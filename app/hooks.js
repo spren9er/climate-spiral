d86033d07b5842d44cc01968eb7ed99a9fabf493
Torsten Sprenger <mail@spren9er.de>
2022-10-18 09:53:48 +0200

    Clean up

--- app/hooks.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ request, resolve }) {
-  const response = await resolve(request, {
-    ssr: false,
-  });
-
-  return response;
-}
